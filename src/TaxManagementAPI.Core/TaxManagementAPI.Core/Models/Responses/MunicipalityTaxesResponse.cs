8648422f51c903d694e61145fb11629a716fa5c1
GintasS <svedgintas@gmail.com>
2021-09-18 23:27:28 +0300

    Added Response classes.

---
+++ src/TaxManagementAPI.Core/TaxManagementAPI.Core/Models/Responses/MunicipalityTaxesResponse.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxManagementAPI.Core.Models.Responses
+{
+    public class MunicipalityTaxesResponse
+    {
+        public string MunicipalityName { get; set; }
+        public DateTime Date { get; set; }
+        public List<TaxModel> Taxes { get; set; }
+    }
+}
---
+++ src/TaxManagementAPI.Core/TaxManagementAPI.Core/Models/Responses/UpdateSingleTaxResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxManagementAPI.Core.Models.Responses
+{
+    public class UpdateSingleTaxResponse : TaxModel
+    {
+    }
+}
