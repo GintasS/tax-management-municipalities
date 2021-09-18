0d31a593f78a624d939c6bbae095d4375ed9ff05
GintasS <svedgintas@gmail.com>
2021-09-18 23:27:09 +0300

    Added Request models.

---
+++ src/TaxManagementAPI.Core/TaxManagementAPI.Core/Models/Requests/UpdateSingleTaxRequest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxManagementAPI.Core.Models.Requests
+{
+    public class UpdateSingleTaxRequest
+    {
+        public int TaxId { get; set; }
+        public TaxModel Tax { get; set; }
+    }
+}
