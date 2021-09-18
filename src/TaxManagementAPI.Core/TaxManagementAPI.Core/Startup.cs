b63ee94d3a31bfe802c5cc380b1286722d2849a1
GintasS <svedgintas@gmail.com>
2021-09-18 23:31:51 +0300

    Updated WebAPI configuration files.

--- src/TaxManagementAPI.Core/TaxManagementAPI.Core/Startup.cs
+++ src/TaxManagementAPI.Core/TaxManagementAPI.Core/Startup.cs
@@ -1,16 +1,14 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using TaxManagementAPI.Core.Configuration;
+using TaxManagementAPI.Core.Interfaces;
+using TaxManagementAPI.Core.Services;
+using TaxManagementAPI.Database;
 
 namespace TaxManagementAPI.Core
 {
@@ -26,12 +24,28 @@ namespace TaxManagementAPI.Core
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "TaxManagementAPI.Core", Version = "v1" });
             });
+
+            ConfigureDatabase(services);
+            RegisterServices(services);
+        }
+
+        private void ConfigureDatabase(IServiceCollection services)
+        {
+            services.AddDbContext<TaxContext>(opt
+                => opt.UseMySQL(Configuration["ApplicationSettings:MySQLConnectionString"]));
+        }
+
+        private void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<ITaxService, TaxService>();
+
+            var appSettings = Configuration.GetSection("ApplicationSettings");
+            services.Configure<ApplicationSettings>(appSettings);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +54,6 @@ namespace TaxManagementAPI.Core
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TaxManagementAPI.Core v1"));
             }
 
             app.UseHttpsRedirection();
@@ -49,6 +61,13 @@ namespace TaxManagementAPI.Core
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Tax Management API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseEndpoints(endpoints =>
             {
