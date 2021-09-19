d5dc8a146e712885ef03a328938e9412f44e80a3
GintasS <svedgintas@gmail.com>
2021-09-19 18:37:10 +0300

    Added requests.

---
+++ src/TaxManagementAPI.Core/TaxManagementAPI.Core/Models/Requests/UpdateMunicipalityNameRequest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxManagementAPI.Core.Models.Requests
+{
+    public class UpdateMunicipalityNameRequest
+    {
+        [Required]
+        public string NewMunicipalityName { get; set; }
+    }
+}
