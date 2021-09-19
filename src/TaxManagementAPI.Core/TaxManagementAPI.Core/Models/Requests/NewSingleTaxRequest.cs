af15dacfbf5e09aa6a035ab6ebcd0adfa5c05baa
GintasS <svedgintas@gmail.com>
2021-09-19 18:37:24 +0300

    Updated requests.

--- src/TaxManagementAPI.Core/TaxManagementAPI.Core/Models/Requests/NewSingleTaxRequest.cs
+++ src/TaxManagementAPI.Core/TaxManagementAPI.Core/Models/Requests/NewSingleTaxRequest.cs
@@ -1,10 +1,17 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using TaxManagementAPI.Database.Entities;
+using TaxManagementAPI.Database.Enums;
 
 namespace TaxManagementAPI.Core.Models.Requests
 {
-    public class NewSingleTaxRequest : TaxModel
+    public class NewSingleTaxRequest
     {
+        public TaxType Type { get; set; }
+        [Required]
+        public TaxRateModel TaxRateModel { get; set; }
+        [Required]
+        public TaxDateModel TaxDateModel { get; set; }
         [JsonIgnore]
         public MunicipalityEntity Municipality { get; set; }
     }
--- src/TaxManagementAPI.Core/TaxManagementAPI.Core/Models/Requests/UpdateSingleTaxRequest.cs
+++ src/TaxManagementAPI.Core/TaxManagementAPI.Core/Models/Requests/UpdateSingleTaxRequest.cs
@@ -1,13 +1,18 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 using TaxManagementAPI.Database.Entities;
+using TaxManagementAPI.Database.Enums;
 
 namespace TaxManagementAPI.Core.Models.Requests
 {
-    public class UpdateSingleTaxRequest : TaxModel
+    public class UpdateSingleTaxRequest
     {
         [JsonIgnore]
         public int TaxId { get; set; }
-        [JsonIgnore]
-        public MunicipalityEntity Municipality { get; set; }
+        public TaxType Type { get; set; }
+        [Required]
+        public TaxRateModel TaxRateModel { get; set; }
+        [Required]
+        public TaxDateModel TaxDateModel { get; set; }
     }
 }
