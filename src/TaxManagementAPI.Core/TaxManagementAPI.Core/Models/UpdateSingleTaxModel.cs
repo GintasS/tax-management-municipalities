275152f007634f3dae840b7c501131fcc12fb06e
GintasS <svedgintas@gmail.com>
2021-09-19 18:36:05 +0300

    Added models.

---
+++ src/TaxManagementAPI.Core/TaxManagementAPI.Core/Models/UpdateSingleTaxModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxManagementAPI.Core.Models
+{
+    public class UpdateSingleTaxModel : TaxModel
+    {
+        public new int TaxId { get; set; }
+    }
+}
---
+++ src/TaxManagementAPI.Core/TaxManagementAPI.Core/Models/UpdateSingleTaxMunicipalityModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaxManagementAPI.Core.Models
+{
+    public class UpdateSingleTaxMunicipalityModel
+    {
+        public int TaxId { get; set; }
+        public MunicipalityModel Municipality { get; set; }
+    }
+}
